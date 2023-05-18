0d5351928c80fcec099b73b6db8b93eb27e96fae
mutaimutai <engineermutai75@gmail.com>
2023-05-18 14:55:57 -0700

    putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return - returns 1
+ */
+
+int main(void)
+{
+	char c = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	putchar (c);
+	return (1);
+}
