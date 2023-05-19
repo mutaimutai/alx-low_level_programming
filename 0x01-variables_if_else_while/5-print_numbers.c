f9f785ba1c544292ba62a06d7621c234c8e2abf7
mutaimutai <engineermutai75@gmail.com>
2023-05-19 07:25:17 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:returns 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (; n <= 9; n++)
+	{
+		putchar('n');
+	}
+putchar('\n');
+return (0);
+}
