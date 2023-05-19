e053a9df28ceb02b0fbb831200b0f0cf54da9a7f
mutaimutai <engineermutai75@gmail.com>
2023-05-19 07:06:27 -0700

    prints alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ * Return:returns 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	for (c >= 'a'; c <= 'z'; c++)
+	{
+	putchar(c);
+	}
+return (0);
+}
