7e7fdeaaaebbb6077ed3de3d6a223d69a8493bf6
mutaimutai <engineermutai75@gmail.com>
2023-05-19 19:06:08 -0700

    combination of 2 numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,8 +8,10 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++) {
-	for (j = i + 1; j < 10; j++) {
+	for (i = 0; i < 10; i++) 
+	{
+	for (j = i + 1; j < 10; j++) 
+	{
 	putchar('0' + i);
 	putchar('0' + j);
 	putchar(',');
