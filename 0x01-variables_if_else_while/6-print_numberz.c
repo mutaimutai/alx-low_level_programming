c496d1be833e4098ea470068cf6e40dd4fe8420e
mutaimutai <engineermutai75@gmail.com>
2023-05-19 18:13:01 -0700

    print binary numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:0 when successfull
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (; n <= 9; n++)
+	{
+		putchar('n');
+	}
+	putchar('\n');
+	return (0);
+}
