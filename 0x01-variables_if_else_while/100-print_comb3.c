5024b8c0a50f775bfe937746bee2b1e8cf82204c
mutaimutai <engineermutai75@gmail.com>
2023-05-19 19:00:50 -0700

    combination of 2 numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return:0 when successfull
+ */
+
+inr main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++) {
+	for (j = i + 1; j < 10; j++) {
+	putchar('0' + i);
+	putchar('0' + j);
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\b');
+	putchar('\b');
+	putchar('\n');
+	return 0;
+}
