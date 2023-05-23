89cbf2e9c274fb76df4ca6cf27e9f1de642673b9
mutaimutai <engineermutai75@gmail.com>
2023-05-23 01:05:29 -0700

    print characters

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-Entry point to the program
+ * Return:Always evaluates to 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
