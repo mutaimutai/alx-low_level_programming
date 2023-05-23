a62a3cded775ddb3a9bfaa7bec3a8c2ddb73751f
mutaimutai <engineermutai75@gmail.com>
2023-05-23 02:38:06 -0700

    prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_alphabet_x10-prints alpabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		char c = 'a';
+
+		for (; c <= z; c++)
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+}
