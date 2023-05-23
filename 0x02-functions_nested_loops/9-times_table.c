c59ba3627e65b8c1d832efdc6b08d88bc994737a
mutaimutai <engineermutai75@gmail.com>
2023-05-23 13:25:05 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * times_table-prints 9 times table
+ * return:0 if it is executed successfully
+ */
+
+void times_table(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (; i <= 9; i++)
+	{
+		for (; j = j + i; j++)
+		{
+			_putchar(i + '0');
+			_putchar(j + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
