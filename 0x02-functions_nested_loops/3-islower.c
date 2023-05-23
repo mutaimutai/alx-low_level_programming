3ec67b36cb2037f25655bd9a68448d1ded8841f7
mutaimutai <engineermutai75@gmail.com>
2023-05-23 03:15:38 -0700

    check if a character is lowercae

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * int _islower-checks if a character is lowercase
+ * Return:0 if successfull 1 if not
+ */
+
+int _islower(int c);
+{
+	char 'a';
+
+	if (c == 'a')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
