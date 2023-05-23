b61fbd0301ff355d4cb9120e2686a9024118d4ce
mutaimutai <engineermutai75@gmail.com>
2023-05-22 19:42:07 -0700

    printing 2 numbers using putchar

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 8; i++) 
+	for (i = 0; i <= 8; i++)
 	{
-	for (j = i + 1; j <= 9; j++) 
+	for (j = i + 1; j <= 9; j++)
 	{
 	putchar(i + '0');
 	putchar(j + '0');
@@ -22,5 +22,5 @@ int main(void)
 	}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
