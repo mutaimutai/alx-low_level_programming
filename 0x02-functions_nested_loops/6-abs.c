bc7f4cabd37257350fbe349ce8d0028c990b98ac
mutaimutai <engineermutai75@gmail.com>
2023-05-23 04:28:15 -0700

    The absolute value of an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs-is a function that computes the absolute value of an interger
+ * @n:the value to be input as argument
+ * Return:the absolute value of an interger
+ */
+
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+		return (-n);
+	return (0);
+}
