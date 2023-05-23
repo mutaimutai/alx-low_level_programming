c6e7b7bcffdb7ffa2317753431c37d0c37ce2f75
mutaimutai <engineermutai75@gmail.com>
2023-05-23 14:31:54 -0700

    prints from 0 to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,15 +8,20 @@
 
 void print_to_98(int n)
 {
-	int i;
-
-	for (i = n; i <= 98; i++)
+	if (n >= 98)
 	{
-		_putchar(i + '0');
-		if (i != 98)
+		while(n > 98)
 		{
-			printf(", ");
+			printf("%d, ", n--);
 		}
-
+		printf("%d\n",n);
+	}
+	else
+	{
+		while(n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
 	}
 }
