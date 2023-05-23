51be71cb632549fb19a691f6577fbce1bb415e0f
mutaimutai <engineermutai75@gmail.com>
2023-05-22 19:51:45 -0700

    Two sets of numbers using putchar

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Its the entry point of the program
+ * Return:Always returns 0 when successfull
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (x = 0; x <=98; x++)
+	{
+		for (y = x + 1; y <= 99; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((X % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+			if (x == 98 && y == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\0');
+	return (0);
+}
