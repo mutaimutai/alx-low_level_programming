b09e25f02b5f884169ec6e18ab95059ecca1865d
mutaimutai <engineermutai75@gmail.com>
2023-05-22 20:11:02 -0700

    printing 3 numbers using putchar

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,17 +4,21 @@
  * Return : always returns 0
  */
 
-int main() {
+int main(void)
+{
 	int i, j, k;
 
-	for (i = 0; i <= 7; i++) {
-		for (j = i + 1; j <= 8; j++) {
-			for (k = j + 1; k <= 9; k++) {
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
-				
-				if (i != 7 || j != 8 || k != 9) {
+				if (i != 7 || j != 8 || k != 9)
+				{
 					putchar(',');
 					putchar(' ');
 				}
@@ -23,6 +27,6 @@ int main() {
 	}
 
 	putchar('\n');
-	return 0;
+	return (0);
 }
 
