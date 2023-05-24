27ef2ca3dedbfa791e826779d9a40362d24ab4f6
mutaimutai <engineermutai75@gmail.com>
2023-05-24 03:54:54 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,13 +8,12 @@
 
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-	{
-		break;
-	}
-
 	int i, j;
 
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
