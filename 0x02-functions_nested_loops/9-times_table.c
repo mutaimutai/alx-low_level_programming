f4e8e9080319f3ac749af38a587f6f7aaeca5546
mutaimutai <engineermutai75@gmail.com>
2023-05-24 02:50:23 -0700

    combination of 2 numbers

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,9 +14,13 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (i == EOF)
+			if (i == 9)
 			{
-			printf("%d,   ", i * j);
+			printf("%d", i * j);
+			}
+			else
+			{
+				printf("%d, ", i * j);
 			}
 		}
 		printf("\n");
