865992c5e7ca436c10dd25b4c47c993978d09c1b
mutaimutai <engineermutai75@gmail.com>
2023-05-25 07:16:20 -0700

    print numbers except

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers-prints numbers except
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	if (i == 2 || i == 4)
+	{
+		for (; i <= 9; i++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}
