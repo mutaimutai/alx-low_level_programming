46f60189fd1c28411e21ed5fde5c590c06954506
mutaimutai <engineermutai75@gmail.com>
2023-05-25 07:40:19 -0700

    prints a number 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers-prints numbers 10 times
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
