71d9173790bc630545676deb07bf6f9753a37677
mutaimutai <engineermutai75@gmail.com>
2023-05-25 04:20:45 -0700

    chhecks for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit-function to check a letter if it is a digit
+ * @c:argument to be passed into the function
+ * Return:1 if true and 0 if it is false
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
