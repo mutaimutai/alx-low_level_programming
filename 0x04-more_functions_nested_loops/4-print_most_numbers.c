dd984b421c433d74c16e91fdf0ed6a74593fb3c9
mutaimutai <engineermutai75@gmail.com>
2023-05-25 07:26:57 -0700

    print numbers except

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,13 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	if (i != 2 || i != 4)
+	for (; i <= 9; i++)
 	{
-		for (; i <= 9; i++)
+		if (i != 2 && i != 4)
 		{
-			_putchar(i + '0');
+			_putchar(i +'0');
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
+
