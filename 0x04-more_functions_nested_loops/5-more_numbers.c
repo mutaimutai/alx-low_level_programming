6d245eac3641d7e15c042192ec7a74371e5712b2
mutaimutai <engineermutai75@gmail.com>
2023-05-25 07:59:04 -0700

    print numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
-			_putchar('\n');
+			if (j > 9)
+			{
+				_putchar('1');
+			}
+			_putchar(j % 10 + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
