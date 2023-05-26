f01f849f1796a48fab6734cdd4096e8e1696efbe
mutaimutai <engineermutai75@gmail.com>
2023-05-25 19:15:27 -0700

    prints a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle-prints a triangle in thhe terminal
+ * @size:argument to be input
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('#');
+		}
+	}
+}
