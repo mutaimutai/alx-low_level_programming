301376c291da1affd611cabb01876e5f20ea8efd
mutaimutai <engineermutai75@gmail.com>
2023-05-29 14:23:10 -0700

    reverses a strung

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * rev_string-reverses a string
  * @s:string to be reversed
@@ -8,6 +9,9 @@ void rev_string(char *s)
 	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
+	{
+		i++;
+	}
 
 	for (j = i - 1; j >= 0; j--)
 	{
