34d86fbdde523e62992446d29ca423e5c7272c6d
mutaimutai <engineermutai75@gmail.com>
2023-05-29 12:41:49 -0700

    length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,12 +2,13 @@
 /**
  * _strlen-functions that checks for string lenth
  * @s:string to be checked
+ * Return:returns the lengh od thr syring
  */
 int _strlen(char *s)
 {
 	int count;
 
-	while (s[count] != '\0')
+	while (*(s + count) != '\0')
 	{
 		count++;
 	}
