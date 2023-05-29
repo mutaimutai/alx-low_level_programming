5283e7137a8e5f3c725ace427748faf646685cfc
mutaimutai <engineermutai75@gmail.com>
2023-05-29 13:22:10 -0700

    prints out a character

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * _puts-function that prints out a string
- * str:argument passed in
+ *_puts-function that prints out a string
+ *@str:argument passed in
  */
 
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	for (; s[i] != '\0'; i++)
 	{
-		putchar(str[i]);
-		i++;
+		_putchar(*(str + i));
 	}
+	_putchar('\n');
 }
