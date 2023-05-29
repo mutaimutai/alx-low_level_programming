7bf0f9cd4e46e7a6cd80da6ec8d18b90ca150d09
mutaimutai <engineermutai75@gmail.com>
2023-05-29 10:06:23 -0700

    using function to change value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - changes the value to 98
+ * @n:pointer to be passed as argument
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+int putchar(int int);
+void reset_to_98(int *n);
