51f0ea28e713b72300966528c38a9fbb77f66046
mutaimutai <engineermutai75@gmail.com>
2023-05-29 13:49:43 -0700

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void print_rev(char *s)
+{
+	int count = 0;
+	int j = 0;
+
+	for (; *(s + count); count++)
+	{
+	}
+
+	for (; j = count - 1; j--)
+	{
+		putchar(s[j]);
+	}
+	putchar('\n');
+}
