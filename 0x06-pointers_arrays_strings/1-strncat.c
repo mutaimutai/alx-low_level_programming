d46ed1afc1e388b7f9b01fb7e24ad0be31620ec4
mutaimutai <engineermutai75@gmail.com>
2023-06-01 05:22:16 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[des_len + i] = src[i];
-		dest[des_len + i] = '\0';
 	}
+	dest[des_len + i] = '\0';
 	return (dest);
 }
