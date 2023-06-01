4924d5abfe2ae28162822399e5e6acea725cb24e
mutaimutai <engineermutai75@gmail.com>
2023-06-01 06:04:20 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#incude "main.h"
+#include <string.h>
+/**
+ * _strncpy-copies a string
+ *@dest:first string
+ *@src:second string
+ *@n:length of strings
+ *Return:final string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '/'; i++)
+	{
+		dest[i] = src[i];
+	}
+	if (; i < n; i++)
+	{ 
+		dest[i] = '\0';
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
 #endif
