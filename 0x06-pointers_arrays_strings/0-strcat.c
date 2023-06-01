204396b80ee15202245f46a54351b285bdeb00f5
mutaimutai <engineermutai75@gmail.com>
2023-06-01 05:04:58 -0700

    add two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcat-concatenates two strings
+ * @dest:sring to be concatenated
+ * @src:the other string
+ * Return:returns the final string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[] = src[i];
+	}
+	return (dest);
