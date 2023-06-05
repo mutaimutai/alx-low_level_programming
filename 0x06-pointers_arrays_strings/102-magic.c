4511d18338bfa4b214c6a6d1b41147dbd139d64d
mutaimutai <engineermutai75@gmail.com>
2023-06-05 09:06:41 -0700

    modifying an array

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+/**
+ * main-entry point of the program
+ * Return:0 when successful
+ */
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int n;
+	int a[5];
+	int *p;
 
-  a[2] = 1024;
-  p = &n;
+	a[2] = 1024;
 
-  *(p + 5) = 98;
+	p = &n;
+
+	*(p + 5) = 98;
 
   /*
    * write your line of code here...
@@ -19,8 +24,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  ;
   /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
