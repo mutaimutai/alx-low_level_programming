463c7ce8148862f0c9bc27e6870066dd7b161448
mutaimutai <engineermutai75@gmail.com>
2023-06-06 17:21:39 -0700

    finds a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr-finds for a character in a string
+ * @s:the string to be checked
+ * @c:the character
+ * Return:returns the resultant string s
+ */
+char *_strchr(char *s, char c)
+{
+	int s_length = strlen(s);
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] = c)
+		{
+			return (s);
+		}
+		return (NULL);
+	}
+}
