53f070520bedf695a050a79ecbf04a2f4cfb646f
Parrot Live user <user@localhost.localdomain>
2023-06-08 12:13:55 +0000

    factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *factorial- is a function that finds factorial of a number
+ *@n:number to be checked
+ *Return:-1 if an error, 1 if n is 0 and returns factorial 
+ */
+int factorial(int n)
+{
+  if (n < 0)
+    return (-1);
+  else if (n == 0)
+    return (1)
+
+      return (n * factorial(n - 1));
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Now we dig down into recursions
