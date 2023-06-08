aeca69681f470bc70b6b72f83d46eba67044cfd0
mutaimutai <engineermutai75@gmail.com>
2023-06-08 06:25:02 -0700

    factorial of a number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,7 +2,7 @@
 /**
  *factorial- is a function that finds factorial of a number
  *@n:number to be checked
- *Return:-1 if an error, 1 if n is 0 and returns factorial 
+ *Return:-1 if an error, 1 if n is 0 and returns factorial
  */
 int factorial(int n)
 {
