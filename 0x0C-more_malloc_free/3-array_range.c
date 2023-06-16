bf3877dba0a7b4753e728da90c6c78c9a55f0952
mutaimutai <engineermutai75@gmail.com>
2023-06-16 07:32:17 -0700

    prints a range of arrays

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range-print a range of intergers from min to max
+ * @min:where to start
+ * @max:where to stop
+ * Return:apointer to the newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	char *ptr;
+	int total;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	total = max - min + 1;
+	ptr = malloc(total *  sizeof(int));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= total; i++)
+	{
+		ptr[i] = min + i;
+	}
+	return (ptr);
+}
