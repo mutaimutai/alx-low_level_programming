2cdd52144b3f3f15b6d0e974dc139c8cb81a8d8e
mutaimutai <engineermutai75@gmail.com>
2023-06-19 11:01:31 -0700

    structure typedef

---
+++ x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+/**
+ * struct dog-ser-defined data type that represents a dog
+ * @name:name of the dog
+ * @age:age of te dog
+ * @owner:owner of the dog
+ */
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
