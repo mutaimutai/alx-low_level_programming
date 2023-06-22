7fc96dd046c7c6fc1cbe9e1466dd1d70b8187220
mutaimutai <engineermutai75@gmail.com>
2023-06-21 18:12:11 -0700

    function ponter to print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * print name- a function that takes in a function pointer as argument
+ * print_name- a function that takes in a function pointer as argument
  * @name:the argument to be printed
  * @f:the function pointer
  */
