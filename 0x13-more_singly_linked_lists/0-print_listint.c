df2be17756b3c96b9ff1d56b6dc100b1a08c0d89
mutaimutai <engineermutai75@gmail.com>
2023-07-04 03:57:06 -0700

    prints elements and number of a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,21 +7,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	const listint_t *ptr = 0;
 
-	if (h == NULL)
-	{
-		printf("list is empty");
-	}
-	ptr = h;
-	if (ptr != NULL)
-	{
 		while (ptr != NULL)
 		{
-			printf("%d\n", ptr->n);
+			printf("%d\n", h->n);
 			count++;
-			ptr = ptr->next;
+			h = h->next;
 		}
-	}
 	return (count);
 }
