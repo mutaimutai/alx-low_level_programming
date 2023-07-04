47922490835fe737d4615c876500c803d587bd4b
mutaimutai <engineermutai75@gmail.com>
2023-07-04 07:54:59 -0700

    add at the begining of the list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,9 +1,15 @@
 #include "lists.h"
+/**
+ * add_nodeint-add a node at the beginning
+ * @head:head pointer
+ * @n:the integer to add
+ * Return:pointer to the added node
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *ptr;
 
-	ptr = (listint_t*)malloc(sizeof(listint_t));
+	ptr = (listint_t *)malloc(sizeof(listint_t));
 	if (ptr == NULL)
 	{
 		printf("memory allocation failed");
