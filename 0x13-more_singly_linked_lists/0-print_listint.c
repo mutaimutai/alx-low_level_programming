8dab8c7509bdbd600a8fe3e63b86643a187caed8
mutaimutai <engineermutai75@gmail.com>
2023-07-04 03:29:05 -0700

    prints number of elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,24 @@
 #include "lists.h"
+/**
+ * print_listint-prints elements of a list
+ * @h:head  node of the list
+ * Return:number of elements printed
+ */
 size_t print_listint(const listint_t *h)
 {
-  size_t count = 0;
-  const listint_t *ptr = 0;
+	size_t count = 0;
+	const listint_t *ptr = 0;
 
-  if (h == NULL)
-  {
-    printf("list is empty");
-  }
-  ptr = h;
-  while (ptr != NULL)
-  {
-    printf("%d\n", ptr->n);
-    count++;
-    ptr = ptr->next;
-  }
-  return (count);
+	if (h == NULL)
+	{
+		printf("list is empty");
+	}
+	ptr = h;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
 }
