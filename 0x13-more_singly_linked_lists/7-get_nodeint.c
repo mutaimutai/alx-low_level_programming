8c6a6265aa448496b4995263fa15c693512dc14b
mutaimutai <engineermutai75@gmail.com>
2023-07-06 02:11:06 -0700

    nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
  * *get_nodeint_at_index-gets the nth node of a list
  * @head:pointer to the head node
  * @index:the index to be returned
- * Reurn:null if the index is not found
+ * Return:null if the index is not found
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
