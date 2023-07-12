fe9d6070cf981f740c467c0a77e354febf03b9a5
mutaimutai <engineermutai75@gmail.com>
2023-07-12 00:52:14 -0700

    create a file and copy content to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	open_f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	open_f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (text_content != NULL)
 	{
 		wr_char = write(open_f, text_content, strlen(text_content));
