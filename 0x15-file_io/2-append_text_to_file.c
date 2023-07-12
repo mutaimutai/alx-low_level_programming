c451108711e1162d82f82b7c43ae10e65ce32830
mutaimutai <engineermutai75@gmail.com>
2023-07-12 03:47:29 -0700

    append a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * append_text_to_file-appends a file to another file
+ * @file:file to be opened
+ * @text_content:the file to be added
+ * Return:the number of characters printed out if successful
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t op, wr_ch;
+
+	if (filename == NULL)
+		return (-1);
+	op = open(filename, O_RDONLY | O_APPEND);
+	
+	if (text_content != NULL)
+	{
+		wr_ch = write(op, text_content, strlen(text_content));
+		if (wr_ch == -1)
+		{
+			close(op);
+			return (-1);
+		}
+	}
+	close(op);
+	return (wr_ch);
+}
